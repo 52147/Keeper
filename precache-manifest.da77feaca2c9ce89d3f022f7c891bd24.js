85e0ca4451f3887de6697bccb03ce16fff7a07e7
Your Name <debra4117@gmail.com>
2022-11-05 04:59:26 -0500

    Updates

---
+++ precache-manifest.da77feaca2c9ce89d3f022f7c891bd24.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "e26c38575c4de17304e8",
+    "url": "/Keeper/static/js/main.e26c3857.chunk.js"
+  },
+  {
+    "revision": "7747ccc47512bfe13170",
+    "url": "/Keeper/static/js/1.7747ccc4.chunk.js"
+  },
+  {
+    "revision": "47e838a1f9c8a8d90fc4",
+    "url": "/Keeper/static/js/runtime~main.47e838a1.js"
+  },
+  {
+    "revision": "3eae5242d8832322c7f761d1a7b23058",
+    "url": "/Keeper/index.html"
+  }
+];
